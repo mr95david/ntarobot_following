2c48b0a1ab413c913f7aea2fad7100821a8c0998
Elio D. Triana R. <75078836+mr95david@users.noreply.github.com>
2024-07-30 21:43:43 -0500

    Home update - two threads
    Update make from home, its necessary test response from robot, and make algorithm for following.

--- src/human_follow.cpp
+++ src/human_follow.cpp
@@ -16,6 +16,7 @@
 // Seccion de librerias utilitarias
 #include <memory>
 #include <iostream>
+#include <thread>
 
 // Seccion de declracion de usings:: Esto corresponde a los placeholders necesarios para los callbacks, 
 // En caso un callback reciba mas de 2 parametros de entrada sera necesario usaro un _2
@@ -23,6 +24,62 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 // Se incluye el using que permite comandos de tiempo como 1s, 10s para ejecucion rapida
 using namespace std::chrono_literals;
+
+// Clase de array cirular cumple con el problema de la necesidad de una lista de 5 posiciones
+class Array {
+    private:
+        double arr[5];
+        int size;
+
+    public:
+        // Constructor: Inicializa el tamaño del array a 0
+        Array() : size(0) {}
+
+        // Método para agregar un valor al array
+        bool add(double value) {
+            // Validacion para agregar un nuevo valor al array
+            if (size < 5) {
+                arr[size] = value;
+                ++size;
+                return true;
+            } else {
+                // En caso que el array este lleno se retorna un valor negativo
+                // std::cerr << "Array is full, cannot add more values.\n";
+                return false;
+            }
+        }
+
+        // Método para obtener y eliminar el primer valor del array
+        bool popFront(double &value) {
+            if (size > 0) {
+                value = arr[0];
+                // Desplazar todos los valores hacia la izquierda
+                for (int i = 1; i < size; ++i) {
+                    arr[i - 1] = arr[i];
+                }
+                --size;
+                return true;
+            } else {
+                // En caso ya no existan valores en el array se retorna un valor negativo
+                // std::cerr << "Array is empty, cannot pop value.\n";
+                return false;
+            }
+        }
+
+        // Metodo para verificar si la lista esta vacia
+        bool isEmpty() const {
+            return size == 0;
+        }
+
+        // Método para imprimir el contenido del array (para propósitos de depuración) - Opcional
+        void print() const {
+            std::cout << "Array contents: ";
+            for (int i = 0; i < size; ++i) {
+                std::cout << arr[i] << " ";
+            }
+            std::cout << "\n";
+        }
+};
 
 // Nodo de ROS2
 class HumanFollowNode :public rclcpp::Node
@@ -49,11 +106,26 @@ class HumanFollowNode :public rclcpp::Node
             ));
 
             // Seccion de creadores de publicadores
-            pub_pose_leida = create_publisher<geometry_msgs::msg::PoseStamped>(
-                "/ntarobot/human_point",
-                10
-            );
+            // pub_pose_leida = create_publisher<geometry_msgs::msg::PoseStamped>(
+            //     "/ntarobot/human_point",
+            //     10
+            // );
+
+            // Creacion de hilo extra para la ejecucion de un action especifico a partir de los valores
+            // existentes de una lista
+            action_thread_ = std::thread([this]() {
+                this->processAction();
+            });
         }
+        // Destructor del nodo
+        ~HumanFollowNode() {
+            running_ = false;
+            cv_.notify_all();
+            if (action_thread_.joinable()) {
+                action_thread_.join();
+            }
+        }
+
     // Seccion de instancias privadas de la clase
     private:
         // INICIO: Seccion de creacion de funciones utilitarias y callbacks necesarias
@@ -71,6 +143,8 @@ class HumanFollowNode :public rclcpp::Node
         );
         // Funcion para llamado de serviro y envio de posicion objetivo
         void call_server(double x, double y);
+        // Funcion para la ejecucion del proceso de navegacion
+        void processAction(void);
         // FINAL
 
         // Inicializacion/declracion de variables
@@ -80,13 +154,25 @@ class HumanFollowNode :public rclcpp::Node
         // FINAL
 
         // INICIO: Seccion de creacion de publicadores
-        rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pub_pose_leida;
+        // rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pub_pose_leida;
         // FINAL
 
         // INICIO: Seccion de creacion de variables de instancia de la calse
         // Primero se crea la variable que corresponde al cliente del servidor de navegacion
         rclcpp_action::Client<NavigateToPose>::SharedPtr navigate_to_pose_client_ptr_;
-        bool validation_Function = false;
+        // variable de pose para deteccion de punto deseado
+        // geometry_msgs::msg::PoseStamped pose;
+        // bool validation_Function = false;
+        // Variable de lista circular para cada uno de los componentes
+        Array x_List;
+        Array y_List;
+
+        // Declaracion de hilo de ejecucion
+        std::thread action_thread_;
+        // variables de ejecucion del hilo
+        std::mutex mutex_;
+        std::condition_variable cv_;
+        bool running_ = true;
         // FINAL
 };
 
@@ -94,35 +180,51 @@ class HumanFollowNode :public rclcpp::Node
 // y lo publica como un nodo
 void HumanFollowNode::pointS_Callback(const geometry_msgs::msg::PointStamped::SharedPtr msg)
 {
-    // Creacion de variable de pose 
-    geometry_msgs::msg::PoseStamped pose;
+    // Validacion unica
+    if (!x_List.add(msg->point.x) || !y_List.add(msg->point.y)){
+        RCLCPP_ERROR(get_logger(), "La lista de datos encuentra llena");
+    } 
+    cv_.notify_all();
+
+    // Se valida y agrega un valor a cada lista con los puntos especificos dados por el subscriber
+    // if (!x_List.add(msg->point.x)) {
+    //     RCLCPP_ERROR(get_logger(), "La lista de valores x esta llena");
+    // }
+    // // Misma validacion para valores de y
+    // if (!y_List.add(msg->point.y)) {
+    //     RCLCPP_ERROR(get_logger(), "La lista de valores y esta llena");
+    // }
+
+    // Visualizacion de listas de coordenadas
+    // x_List.print();
+    // y_List.print();
 
     // Asignacion de valores de mensaje obtenidos por la lectura de pose
-    pose.header = msg->header;
-    // Datos de posicion
-    pose.pose.position.x = msg->point.x;
-    pose.pose.position.y = msg->point.y;
-    pose.pose.position.z = msg->point.z;
-    // Valor de orientacion estimada
-    pose.pose.orientation.x = 0;
-    pose.pose.orientation.y = 0;
-    pose.pose.orientation.z = 0;
-    pose.pose.orientation.w = 1;
+    // pose.header = msg->header;
+    // // Datos de posicion
+    // pose.pose.position.x = msg->point.x;
+    // pose.pose.position.y = msg->point.y;
+    // pose.pose.position.z = msg->point.z;
+    // // Valor de orientacion estimada
+    // pose.pose.orientation.x = 0;
+    // pose.pose.orientation.y = 0;
+    // pose.pose.orientation.z = 0;
+    // pose.pose.orientation.w = 1;
 
     // Publicacion de pose leida
-    pub_pose_leida->publish(pose);
-
+    // pub_pose_leida->publish(pose);
     // ejecccion se solicitud de servicio
     // RCLCPP_ERROR(get_logger(), "Ejecutando servicio - puerta 1");
-    if (!validation_Function){
-        call_server(msg->point.x, msg->point.y);
-    };
+    
+    // if (!validation_Function){
+    //     call_server(msg->point.x, msg->point.y);
+    // };
 }
 
 // La siguiente funcion realiza el llamado al servidor de manera que publica la 
 // navegacion hasta el punto especificado.
 void HumanFollowNode::call_server(double x, double y) {
-    this->validation_Function = true;
+    // this->validation_Function = true;
     // RCLCPP_ERROR(get_logger(), "Ejecutando servicio - puerta 2");
     // Se usa la variable que corresponde al cliente creada anteriormente
     navigate_to_pose_client_ptr_ = 
@@ -191,7 +293,7 @@ void HumanFollowNode::result_callback(const GoalHandleNavigateToPose::WrappedRes
     switch (result.code) {
         case rclcpp_action::ResultCode::SUCCEEDED:
             RCLCPP_INFO(get_logger(), "Success!!!");
-            this->validation_Function = false;
+            // this->validation_Function = false;
             break;
         case rclcpp_action::ResultCode::ABORTED:
             RCLCPP_ERROR(get_logger(), "El objetivo fue abortado");
@@ -202,6 +304,31 @@ void HumanFollowNode::result_callback(const GoalHandleNavigateToPose::WrappedRes
         default:
             RCLCPP_ERROR(get_logger(), "Código de resultado desconocido");
             return;
+    }
+}
+
+// Funcion de ejecucion de process action
+void HumanFollowNode::processAction(void) {
+    // Ejecucion continua del hilo mientras la lista tenga datos
+    while (rclcpp::ok() && running_) {
+        // Bloqueo del hilo
+        RCLCPP_ERROR(get_logger(), "Estancia de prueba 1");
+        std::unique_lock<std::mutex> lock(mutex_);
+        cv_.wait(lock, [this]() { return !x_List.isEmpty() || !running_; });
+        RCLCPP_ERROR(get_logger(), "Estancia de prueba 2");
+        // Declaracion de valores actuales
+        double x_actual;
+        double y_actual;
+        RCLCPP_ERROR(get_logger(), "Estancia de prueba 3");
+        // Inicializacion de valores actuales
+        while (!x_List.isEmpty() || !y_List.isEmpty()) {
+            x_List.popFront(x_actual);
+            y_List.popFront(y_actual);
+            RCLCPP_ERROR(get_logger(), "Estancia de prueba 4");
+            lock.unlock(); 
+            call_server(x_actual, y_actual);
+            lock.lock();
+        }
     }
 }
 
